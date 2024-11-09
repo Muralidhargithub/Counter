6f706baaa851c97b364b4a37bb1e95ebdff5dbd9
Muralidhargithub <ot41975@umbc.edu>
2024-11-08 19:11:16 -0500

    Counter project--version-1.0

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counter: UILabel!
+    var count:Int=0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter.text="\(count)"
     }
 
-
+    @IBAction func Increase(_ sender: Any) {
+        count+=1
+        counter.text="\(count)"
+    }
+    
+    @IBAction func Decrease(_ sender: Any) {
+        count-=1
+        counter.text="\(count)"
+        
+    }
 }
 
