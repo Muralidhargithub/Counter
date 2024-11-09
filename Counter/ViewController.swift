4067b1c618cb5e42224f3459e9d6c58b2dfc6fc9
Muralidhargithub <ot41975@umbc.edu>
2024-11-09 01:15:05 -0500

    Added reset button and min and max limit for the counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Muralidhar reddy Kakanuru on 11/8/24.
+//  Created by Muralidhar Reddy Kakanuru on 11/8/24.
 //
 
 import UIKit
@@ -10,22 +10,54 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counter: UILabel!
-    var count:Int=0
+    @IBOutlet weak var startingValueField: UITextField! // Connect your text field
+
+    var count: Int = 0 {
+        didSet {
+            counter.text = "\(count)"
+        }
+    }
+    
+    let maxValue = 50
+    let minValue = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        counter.text="\(count)"
+        counter.text = "\(count)"
     }
 
-    @IBAction func Increase(_ sender: Any) {
-        count+=1
-        counter.text="\(count)"
+    @IBAction func startvalue(_ sender: UIButton) {
+        if let inputValue = startingValueField.text, let newValue = Int(inputValue) {
+            count = newValue
+        } else {
+            showAlert(message: "Please enter a valid number.")
+        }
     }
-    
-    @IBAction func Decrease(_ sender: Any) {
-        count-=1
-        counter.text="\(count)"
-        
+
+    @IBAction func Increase(_ sender: UIButton) {
+        if count < maxValue {
+            count += 1
+        } else {
+            showAlert(message: "Maximum value reached!")
+        }
+    }
+
+    @IBAction func reset(_ sender: UIButton) {
+        count = 0
+    }
+
+    @IBAction func Decrease(_ sender: UIButton) {
+        if count > minValue {
+            count -= 1
+        } else {
+            showAlert(message: "Minimum value reached!")
+        }
+    }
+
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
-
